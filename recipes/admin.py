21418b3aaebb3bdf38da01df410dd1522f2c6aa4
propavi <pavithran@testpress.in>
2025-08-07 17:12:13 +0530

    Registered models in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import User,Recipe, Collection, Ingredient  # Add your models here
 
-# Register your models here.
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cuisine', 'difficulty', 'veg_type', 'author', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('cuisine', 'difficulty', 'veg_type')
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'unit', 'optional', 'recipe')
+    search_fields = ('name',)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    search_fields = ('name', 'user__username')
+    filter_horizontal = ('recipes',)
+
+admin.site.register(User)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Collection, CollectionAdmin)
+
