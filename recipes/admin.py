f31991575f0d13158365f04540c43af9a21f987d
Pavithran-tp <pavithran@testpress.in>
2025-08-08 11:54:06 +0530

    feat: Register models in admin panel (#2)
    * Add models and initial migration
    
    * Add models and initial migration
    
    * Registered models in admin.py
    
    * Changed inbuilt user model to Custom User model
    
    * Removed initial migration from PR
    
    * Restore 0001_initial.py from main into feature/admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User,Recipe, Collection, Ingredient  
 
-# Register your models here.
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cuisine', 'difficulty', 'veg_type', 'author', 'created_at')
+    search_fields = ('title', 'description')
+    list_filter = ('cuisine', 'difficulty', 'veg_type')
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'unit', 'optional', 'recipe')
+    search_fields = ('name',)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    search_fields = ('name', 'user__username')
+    filter_horizontal = ('recipes',)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Profile', {'fields': ('is_chef', 'bio')}),
+    )
+    list_display = list(UserAdmin.list_display) + ['is_chef']
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Collection, CollectionAdmin)
+
