4c34075bf070b5941965c2a2c029bf093cfc0c38
propavi <pavithran@testpress.in>
2025-08-08 15:37:34 +0530

    add is_chef and bio to add_fieldsets and clarify M2M fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,9 +3,11 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 class CustomUserAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Profile', {'fields': ('is_chef', 'bio')}),
+    )
     fieldsets = UserAdmin.fieldsets + (
         ('Custom Profile', {'fields': ('is_chef', 'bio')}),
     )
-    list_display = list(UserAdmin.list_display) + ['is_chef']
 
 admin.site.register(User, CustomUserAdmin)
