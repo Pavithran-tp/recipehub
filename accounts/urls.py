e7c62ea9bf8a33f710856aeceb0818c019391911
propavi <pavithran@testpress.in>
2025-08-20 14:12:19 +0530

    Added password change functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignUpView
-from accounts.views import CustomLoginView
+from .views import SignUpView, CustomLoginView, CustomPasswordChangeView, CustomPasswordChangeDoneView
+
+app_name = "accounts"
 
 app_name = "accounts"
 
@@ -9,4 +10,6 @@ urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'), 
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import CreateView
 from .models import User
 from .forms import CustomUserCreationForm
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 class SignUpView(CreateView):
     model = User
@@ -12,3 +13,12 @@ class SignUpView(CreateView):
 
 class CustomLoginView(LoginView):
     template_name = "accounts/login.html"
+
+class CustomPasswordChangeView(auth_views.PasswordChangeView):
+    template_name = 'accounts/password_change.html'
+    success_url = reverse_lazy('accounts:password_change_done')
+
+class CustomPasswordChangeDoneView(auth_views.PasswordChangeDoneView):
+    template_name = 'accounts/password_change_done.html'
+
+
