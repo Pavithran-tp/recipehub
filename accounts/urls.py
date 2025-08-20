a001882b7a8df4dcbddf2428227766d98702af0d
propavi <pavithran@testpress.in>
2025-08-20 14:26:03 +0530

    Updated accounts urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 from .views import SignUpView
 from accounts.views import CustomLoginView
 
+app_name = "accounts"
+
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'), 
     path('login/', CustomLoginView.as_view(), name='login'),
