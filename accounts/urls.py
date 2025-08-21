1e4b34ca6aeb366d2e375b99dfccf89be6d36d02
propavi <pavithran@testpress.in>
2025-08-21 16:09:00 +0530

    Updated login.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,8 @@ app_name = "accounts"
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'), 
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password-change/', CustomPasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', CustomPasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('', include('django.contrib.auth.urls')),
 ]
