d142ec94eed01176f7dd1308888e050464abb226
propavi <pavithran@testpress.in>
2025-08-21 13:01:20 +0530

    Updated urls.py and templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
 from .views import SignUpView, CustomLoginView, CustomPasswordChangeView, CustomPasswordChangeDoneView
 
 app_name = "accounts"
