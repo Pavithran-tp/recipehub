b48993118e265e8c094f4ae9378155170dec9a8e
Pavithran-tp <pavithran@testpress.in>
2025-08-26 14:33:46 +0530

    updated recipes/views.py

--- recipes/views.py
+++ recipes/views.py
@@ -104,6 +104,7 @@ class CreateRecipeView(LoginRequiredMixin,CreateView):
                 self.object = form.save(commit=False)
                 self.object.author = self.request.user
                 self.object.save()
+                ingredient_formset.instance = self.object
                 ingredient_formset.save()
                 return super().form_valid(form)
             else:
@@ -136,7 +137,6 @@ class UpdateRecipeView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         with transaction.atomic():
             if form.is_valid() and ingredient_formset.is_valid():
                 self.object = form.save()
-                ingredient_formset.instance = self.object
                 ingredient_formset.save()
                 return redirect(self.get_success_url())
             else:
