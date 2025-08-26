fc09ccea3071548a8c9dd98fd01f1eee77118760
Pavithran-tp <pavithran@testpress.in>
2025-08-26 15:49:11 +0530

    Modified changes according to the code review

--- recipes/views.py
+++ recipes/views.py
@@ -76,13 +76,9 @@ class RecipeDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
-            from recipe_collections.models import Collection
             user_collections = Collection.objects.filter(user=self.request.user)
             context['recipe_collections'] = user_collections
-            recipe_in_collections = user_collections.filter(recipes=self.object)
-            context['recipe_in_collections_ids'] = [
-                collection.id for collection in recipe_in_collections
-            ]
+            context['recipe_in_collections_ids'] = set(user_collections.filter(recipes=self.object).values_list('id', flat=True))
         return context
 
 
