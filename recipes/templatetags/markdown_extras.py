6102f5813fec42b7bd592e1abea91b6ae1861f1b
Pavithran-tp <pavithran@testpress.in>
2025-08-29 10:46:10 +0530

    Updated markdown_extras.py

--- recipes/templatetags/markdown_extras.py
+++ recipes/templatetags/markdown_extras.py
@@ -1,10 +1,8 @@
 import markdown
-import bleach
 from django import template
 
 register = template.Library()
 
 @register.filter
 def markdownify(text):
-    html = markdown.markdown(text, extensions=["fenced_code", "tables"])
-    return bleach.clean(html)
+    return markdown.markdown(text, extensions=["fenced_code", "tables"])
